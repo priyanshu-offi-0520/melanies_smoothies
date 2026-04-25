52935525b934ea38dc79e71dd4d83596fa338a2a
Priyanshu Goyal <priyanshugoyal0520.official@gmail.com>
2026-04-25 17:56:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -11,19 +10,8 @@ st.write(
 name_on_order = st.text_input('Name on smoothie: ')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-# option = st.selectbox(
-#     'How would you like to be contacted?',
-#     ('Email', 'Home phone', 'Mobile phone'))
-
-# st.write('You selected:', option)
-
-# option = st.selectbox(
-#     'What is your favorite fruit?',
-#     ('Banana', 'Strawberries', 'Peaches'))
-
-# st.write('Your favorite fruit is:', option)
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
